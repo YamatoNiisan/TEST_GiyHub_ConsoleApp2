861e58a3bb3b7b8464528732ef2a393ee4ad3bab
YamatoNiisan <43798749+YamatoNiisan@users.noreply.github.com>
2020-09-25 11:56:53 +0900

    新規作成

---
+++ TEST_GiyHub_ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST_GiyHub_ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
